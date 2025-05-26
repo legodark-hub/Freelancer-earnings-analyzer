9daada9805a83501e4d3ecc8ab8094b03d4f0157
aidarminyazev <legodark@mail.ru>
2025-05-26 08:30:34 +0500

    remove unused stuff

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ import os
 
 load_dotenv()
 
-DB_DIR = os.getenv("DB_DIR")
-MODEL_NAME = os.getenv("MODEL_NAME")
 CSV_PATH = os.getenv("CSV_PATH")
 LLM_BASE_URL = os.getenv("LLM_BASE_URL")
 LLM_API_KEY = os.getenv("LLM_API_KEY")
